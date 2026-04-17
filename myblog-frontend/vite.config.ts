deee7a3199c1346fd8003e51e2219aed31f8492e
zzq <1811657977@qq.com>
2026-04-17 09:23:30 +0800

    [init] fix init bug

--- myblog-frontend/vite.config.ts
+++ myblog-frontend/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, '.', '');
 
   return {
     plugins: [vue()],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': '/src'
       }
     },
     server: {
